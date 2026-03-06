6f7d1f1b0d72c4c0315ef82fa44978d37176f609
MD MANSOOR ALAM <143305156+mdmansooralam@users.noreply.github.com>
2026-03-06 17:47:31 +0530

    AI generated code update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 import './App.css';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/todos';
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/todos')
-      .then(res => setTodos(res.data))
-      .catch(err => console.log(err));
+    getTodos();
   }, []);
 
-  const addTodo = (text) => {
-    axios.post('/api/todos', { text })
-      .then(res => setTodos([res.data, ...todos]))
-      .catch(err => console.log(err));
+  const getTodos = async () => {
+    try {
+      const res = await axios.get(API_URL);
+      setTodos(res.data);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  const toggleComplete = (id) => {
-    axios.put(`/api/todos/${id}`)
-      .then(res => {
-        setTodos(todos.map(todo => 
+  const addTodo = async (text) => {
+    try {
+      const res = await axios.post(API_URL, { text });
+      setTodos([res.data, ...todos]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const toggleComplete = async (id) => {
+    try {
+      const res = await axios.put(`${API_URL}/${id}`);
+      setTodos(
+        todos.map((todo) =>
           todo._id === id ? { ...todo, completed: res.data.completed } : todo
-        ));
-      })
-      .catch(err => console.log(err));
+        )
+      );
+    } catch (err) {
+      console.error(err);
+    }
   };
 
-  const deleteTodo = (id) => {
-    axios.delete(`/api/todos/${id}`)
-      .then(() => {
-        setTodos(todos.filter(todo => todo._id !== id));
-      })
-      .catch(err => console.log(err));
+  const deleteTodo = async (id) => {
+    try {
+      await axios.delete(`${API_URL}/${id}`);
+      setTodos(todos.filter((todo) => todo._id !== id));
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
-    <div className="App">
+    <div className="app">
       <div className="container">
-        <h1>Todo App</h1>
+        <h1 className="text-center mb-4">Indixpert Todo application</h1>
         <TodoForm addTodo={addTodo} />
-        <TodoList todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+        <TodoList
+          todos={todos}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+        />
       </div>
     </div>
   );
